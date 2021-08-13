ab3992898fb461869241b1a0c5bc3c6fa1539e90
Mateusz Filipek <mateusz.filipek@softwareplant.com>
2021-08-13 13:43:04 +0200

    Output readability improvement for scripts which returns builds started by users

--- GetBuildsStartedByUser.groovy
+++ GetBuildsStartedByUser.groovy
@@ -3,6 +3,6 @@ def jobFullName = "PPM/PERFORMANCE/PPM_PERF_TEST_E2E"
 def builds = Jenkins.instance.getItemByFullName(jobFullName).getBuilds().findAll {build -> build.getCause(hudson.model.Cause$UserIdCause) != null }
 
 for (def build : builds){
-	println build
-	println build.getCause(hudson.model.Cause$UserIdCause).getUserName()
+	println "${build} was started by - ${build.getCause(hudson.model.Cause$UserIdCause).getUserName()}"
+	println "============================================================"
 }
