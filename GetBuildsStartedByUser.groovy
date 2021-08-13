35c9c39e74c8efde1e7ab6a9947c5798c1ae6091
Mateusz Filipek <mateusz.filipek@softwareplant.com>
2021-08-13 13:38:47 +0200

    Adding script for getting builds started by user

---
+++ GetBuildsStartedByUser.groovy
@@ -0,0 +1,8 @@
+def jobFullName = "PPM/PERFORMANCE/PPM_PERF_TEST_E2E"
+
+def builds = Jenkins.instance.getItemByFullName(jobFullName).getBuilds().findAll {build -> build.getCause(hudson.model.Cause$UserIdCause) != null }
+
+for (def build : builds){
+	println build
+	println build.getCause(hudson.model.Cause$UserIdCause).getUserName()
+}
