b7afb35962f6b0d2055b414e9cf511b3acf0b7cf
Mateusz Filipek <mateusz.filipek@softwareplant.com>
2021-09-22 15:54:28 +0200

    Commit message

--- scripts/GetBuildsStartedByUser.groovy
+++ scripts/GetBuildsStartedByUser.groovy
@@ -1,4 +1,4 @@
-def jobFullName = "PPM/PERFORMANCE/PPM_PERF_TEST_E2E"
+def jobFullName = "JOB_FULL_NAME"
 
 def builds = Jenkins.instance.getItemByFullName(jobFullName).getBuilds().findAll {build -> build.getCause(hudson.model.Cause$UserIdCause) != null }
 
